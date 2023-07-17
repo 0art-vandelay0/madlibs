2c5aa919aa2ec8482fa00ae081d3c25f64a3a227
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-17 11:41:49 -0700

    Add routes

--- Madlibs/Contollers/HomeController.cs
+++ Madlibs/Contollers/HomeController.cs
@@ -5,7 +5,18 @@ namespace Madlibs.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/form")]
+        [Route("/")]
         public ActionResult Form() { return View(); }
+
+        [Route("/finalMadlib")]
+        public ActionResult FinalMadlib(string noun, string verb, string adjective, string adverb)
+        {
+            MadlibVariable myMadlibVariable = new MadlibVariable();
+            myMadlibVariable.Noun = noun;
+            myMadlibVariable.Verb = verb;
+            myMadlibVariable.Adjective = adjective;
+            myMadlibVariable.Adverb = adverb;
+            return View(myMadlibVariable);
+        }
     }
 }
